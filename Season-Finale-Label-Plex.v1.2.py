bf0089b90ab0b062dd65dcd4d2af98627baca68d
netplexflix <97123790+netplexflix@users.noreply.github.com>
2025-01-09 11:22:42 +0100

    Update Season-Finale-Label-Plex.v1.2.py

--- Season-Finale-Label-Plex.v1.2.py
+++ Season-Finale-Label-Plex.v1.2.py
@@ -471,7 +471,7 @@ if __name__ == "__main__":
         print(BLUE + f"No finales aired in the last {RECENT_DAYS} days (or all were skipped by genre, label, and unwatched condition)." + RESET)
     else:
         if filtered_downloaded:
-            print(GREEN + f"Downloaded Finales in the Last {RECENT_DAYS} Days ({len(filtered_downloaded)}):" + RESET)
+            print(GREEN + f"Downloaded Finales in the Last {RECENT_DAYS} Days matching criteria ({len(filtered_downloaded)}):" + RESET)
             for finale in filtered_downloaded:
                 if len(finale) == 9:
                     title, snum, enum, ep_title, air_date, tmdb_id, imdb_id, monitored, is_future = finale
@@ -486,7 +486,7 @@ if __name__ == "__main__":
                 print(line)
 
         if filtered_not_downloaded:
-            print(ORANGE + f"\nNot Downloaded Finales in the Last {RECENT_DAYS} Days ({len(filtered_not_downloaded)}):" + RESET)
+            print(ORANGE + f"\nNot Downloaded Finales in the Last {RECENT_DAYS} Days  ({len(filtered_not_downloaded)}):" + RESET)
             for finale in filtered_not_downloaded:
                 if len(finale) == 9:
                     title, snum, enum, ep_title, air_date, tmdb_id, imdb_id, monitored, is_future = finale
